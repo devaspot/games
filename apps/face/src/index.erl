c611fdc197c205e5e7e004a01bc3c96d67b36f1f
Roman Dayneko <roman.dayneko@gmail.com>
2014-04-08 10:42:03 +0300

    sync

--- apps/face/src/index.erl
+++ apps/face/src/index.erl
@@ -21,6 +21,23 @@ take(GameId,Place) ->
         bert:atom('client'),
         bert:tuple(bert:atom("game_action"),GameId,bert:atom("okey_take"),[{pile,Place}])))).
 
+%%discard(GameId, Value, Color) ->
+%%    ws:send(
+%%      bert:encodebuf(
+%%        bert:tuple(
+%%          bert:atom('client'),
+%%          bert:tuple(
+%%            bert:atom("game_action"),GameId,bert:atom("okey_discard"),
+%%            [bert:atom()]
+%%            [{pile,Place}]
+%%           )
+%%         )
+%%       )
+%%     ).
+
+redraw_tiles(TilesList) ->
+    wf:replace(drop, #dropdown{id = drop, options = [#option{label = VCBin} || {VCBin, _} <- TilesList]}).
+
 main() -> #dtl{file="index", bindings=[{title,<<"N2O">>},{body,body()}]}.
 
 body() ->
@@ -44,4 +61,15 @@ event(combo)  -> wf:info("Combo: ~p",[wf:q(drop)]);
 event(join)   -> wf:wire(join("1000001"));
 event(attach) -> wf:wire(attach("'"++?TEST_TOKEN++"'"));
 event(take)   -> wf:wire(take("1000001","0"));
+event({game_event, _, okey_game_started, Args}) ->
+    {_, Tiles} = lists:keyfind(tiles, 1, Args),
+    TilesList = [{erlang:list_to_binary([erlang:integer_to_list(V), " ", erlang:integer_to_list(C)]), {V, C}} || {_, V, C} <- Tiles],
+    put(game_okey_tiles, TilesList),
+    redraw_tiles(TilesList),
+    wf:info("tiles ~p", [Tiles]);
+event({game_event, _, okey_tile_discarded, Args}) ->
+    {_, {_, V, C}} = lists:keyfind(tile, 1, Args),
+    TilesListOld = get(game_okey_tiles),
+    TilesList = lists:keydelete()
+    ;
 event(Event)  -> wf:info("Event: ~p", [Event]).
