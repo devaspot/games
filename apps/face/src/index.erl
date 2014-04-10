4bcf01f0eb51d6c21d89b790c8df11665a2349f1
Maxim Sokhatsky <maxim@synrc.com>
2014-04-10 20:38:54 +0300

    autojoin

--- apps/face/src/index.erl
+++ apps/face/src/index.erl
@@ -65,6 +65,8 @@ body() ->
 event(terminate) -> wf:info("terminate");
 event(init) ->
     {ok,GamePid} = game_session:start_link(self()),
+    event(attach),
+    event(join),
     wf:info("INIT ~p",[GamePid]),
     put(game_session, GamePid);
 
