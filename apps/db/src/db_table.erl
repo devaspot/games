85cfc5bf66455cb2e3da59416b451bde697a734d
Roman Dayneko <roman.dayneko@gmail.com>
2014-04-10 14:04:36 +0300

    fix typo in filename

---
+++ apps/db/src/db_table.erl
@@ -0,0 +1,12 @@
+-module(db_table).
+-include_lib("kvs/include/metainfo.hrl").
+-include_lib("db/include/table.hrl").
+-compile(export_all).
+
+metainfo() ->
+    #schema{name = kvs, tables = 
+                [
+                 #table{name = game_table, fields=record_info(fields, game_table)},
+                 #table{name = save_game_table, fields=record_info(fields, save_game_table)}
+                ]
+           }.
