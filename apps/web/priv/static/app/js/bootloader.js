1b4185ae84ea0ddad239dc5927fdff60e100d654
Maxim Sokhatsky <maxim@synrc.com>
2014-06-13 04:15:36 +0300

    pause colors #21

--- apps/web/priv/static/app/js/bootloader.js
+++ apps/web/priv/static/app/js/bootloader.js
@@ -199,9 +199,9 @@ function hours() { return (new Date()).getHours(); }
 
 function initPauseOverlay() {
     var html = '<g xmlns="http://www.w3.org/2000/svg" id="overlay" style="display:none;">'+
-        '<rect x="216" y="91" stroke-width="0" stroke="red" width="641" height="367" rx="6" fill="white" opacity="0.7"></rect>'+
+        '<rect x="216" y="91" stroke-width="0" stroke="red" width="641" height="367" rx="6" fill="skyblue" opacity="0.8"></rect>'+
         '<g>'+
-        '<text fill="#3B5998" font-family="Exo 2" y="280" x="-116" text-anchor="middle" dx="641" font-size="26pt"> Someone paused the game</text></g>'+
+        '<text fill="white" font-family="Exo 2" y="280" x="-116" text-anchor="middle" dx="641" font-size="30pt"> Someone paused the game</text></g>'+
         '</g>';
     var page = document.getElementById("Kakaranet-12-maxim");
     var kakush = document.getElementById("Kakush");
