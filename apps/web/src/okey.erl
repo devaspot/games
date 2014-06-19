ebcf43f6af414662d30cb05290835caea6512af5
Maxim Sokhatsky <maxim@synrc.com>
2014-06-19 17:21:54 +0300

    sort users right way

--- apps/web/src/okey.erl
+++ apps/web/src/okey.erl
@@ -102,7 +102,7 @@ send_roster(Pid) ->
     X = [ begin
        {User#user.id,User#user.names,User#user.surnames,score(User)}
        end || User=#user{tokens=Tokens} <- kvs:all(user), Tokens /= [], Tokens /= undefined, proplists:get_value(score,Tokens,0) /= 0],
-    XS = lists:sort(fun({_,_,_,S1},{_,_,_,S2}) -> S1 < S2 end,X),
+    XS = lists:sort(fun({_,_,_,S1},{_,_,_,S2}) -> S1 > S2 end,X),
     Lists = [lists:sublist(XS,100)], %split(170,XS,[]),
     [ send_roster_group(Pid,List) || List <- Lists],
     Pid ! {server,{roster_end,length(Lists)}},
